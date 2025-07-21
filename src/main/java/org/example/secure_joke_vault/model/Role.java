f974a448f4537535241ad3d5f0abb08e435ba5fd
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-21 12:58:17 +0200

    completed User model with Enum Role

---
+++ src/main/java/org/example/secure_joke_vault/model/Role.java
@@ -0,0 +1,11 @@
+package org.example.secure_joke_vault.model;
+
+/**
+ * Enum representing possible roles a user can have.
+ */
+public enum Role {
+    USER,
+    ADMIN,
+    MODERATOR,
+    SUPER_ADMIN
+}
