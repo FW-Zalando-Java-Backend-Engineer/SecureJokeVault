f5f413a2d7d9c0013fe52cfacc8be15f4f536b97
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-21 11:45:44 +0200

    init commit

---
+++ src/main/java/org/example/secure_joke_vault/SecureJokeVaultApplication.java
@@ -0,0 +1,13 @@
+package org.example.secure_joke_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureJokeVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureJokeVaultApplication.class, args);
+	}
+
+}
