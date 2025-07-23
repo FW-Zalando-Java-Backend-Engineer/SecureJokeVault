e34b2444935ca1f8e7b0b47cf87b890efde08fe6
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-23 11:33:53 +0200

    completed UserDetialsServiceImpl class

--- src/main/java/org/example/secure_joke_vault/alias/SecurityUser.java
+++ src/main/java/org/example/secure_joke_vault/alias/SecurityUser.java
@@ -6,11 +6,30 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 
 public class SecurityUser extends User {
-    public SecurityUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
+    public SecurityUser(
+            String username,
+            String password,
+            Collection<? extends GrantedAuthority> authorities) {
+                     super(  username,
+                             password,
+                             authorities
+                     );
     }
 
-    public SecurityUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    public SecurityUser(
+            String username,
+            String password,
+            boolean enabled,
+            boolean accountNonExpired,
+            boolean credentialsNonExpired,
+            boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+                     super(  username,
+                             password,
+                             enabled,
+                             accountNonExpired,
+                             credentialsNonExpired,
+                             accountNonLocked,
+                             authorities
+                     );
     }
 }
