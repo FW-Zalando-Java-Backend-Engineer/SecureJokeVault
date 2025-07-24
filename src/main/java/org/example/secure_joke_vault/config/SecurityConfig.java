3517d5eb3708ae141dfda9d2385de8861332f470
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-24 10:09:52 +0200

    started the class SecurityConfig , completed the method filterChain

---
+++ src/main/java/org/example/secure_joke_vault/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.example.secure_joke_vault.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.secure_joke_vault.security.JWTFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Main Spring Security configuration.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JWTFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        // CSRF: Cross Site Request Forgery: famous attack
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable) // JWT is stateless (no session-based auth) so csrf is no applicable.
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtFilter, UsernamePasswordAuthenticationFilter.class
+                );
+        return httpSecurity.build(); // builds and returns the configured SecurityFilterChain.
+    }
+
+
+}
